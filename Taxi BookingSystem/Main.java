ee4ea357779238c862a42245d2d0f7efc1228ee2
Bhoomish V A <157049331+bhoomish-v-a@users.noreply.github.com>
2025-03-06 22:34:16 +0530

    Update Main.java

--- Taxi BookingSystem/Main.java
+++ Taxi BookingSystem/Main.java
@@ -4,8 +4,8 @@ class TaxiDetails{
     String name;
     boolean currentStatus;
     String currentLocation;
-    double arrvingTime;
-    String arrivingLocation;
+    //double arrvingTime;
+    //String arrivingLocation;
                 
     TaxiDetails(int id,String name,String loc){
         this.currentLocation=loc;
@@ -19,9 +19,14 @@ class TaxiAllocation{
   
     Scanner s=new Scanner(System.in);
     void addDetails(){
-        System.out.println("Enter car name\n Current Loaction of the taxi\n");
-        TaxiDetails td=new TaxiDetails(taxi.size()+1,s.nextLine(),s.nextLine());
+        System.out.println("Enter car name");
+        String a=s.nextLine();
+        System.out.println("Current Loaction of the taxi");
+        String b=s.nextLine();
+        TaxiDetails td=new TaxiDetails(taxi.size()+1,a,b);
+        //System.out.println(td.name);
         taxi.add(td);
+        //System.out.println(taxi.size());
     }
 }
 class TaxiBooking extends TaxiAllocation{
@@ -29,7 +34,7 @@ class TaxiBooking extends TaxiAllocation{
     private String dispachLocation;
     private String distinationLocation;
     private double dispachTime;
-    
+
     Scanner s=new Scanner(System.in);
     TaxiBooking(String uname){
         this.uname=uname;
@@ -55,28 +60,51 @@ class TaxiBooking extends TaxiAllocation{
         }
         return true;
     }
-    private void getTravelDetails(){
-        System.out.println("Enter Dispach loaction\nDistination Location\nDispach time");
-        this.dispachLocation=s.nextLine();
-        this.distinationLocation=s.nextLine();
-        this.dispachTime=s.nextDouble();
+    private void getTravelDetails() {
+        System.out.println("Enter Dispatch location:");
+        this.dispachLocation = s.next(); 
+        System.out.println("Enter Dispatch time:");
+        while (!s.hasNextDouble()) { 
+            System.out.println("Invalid input! Please enter a valid number for dispatch time:");
+            s.next(); 
+        }
+        this.dispachTime = s.nextDouble();
+        s.nextLine(); 
+    
+        System.out.println("Enter Destination Location:");
+        this.distinationLocation = s.nextLine(); // Read destination location
     }
     void bookTaxi(){
-        
+        for(TaxiDetails x:taxi){
+            System.out.println(x.currentLocation+"   "+this.dispachLocation);
+            if(x.currentLocation==this.dispachLocation&&x.currentStatus==false){
+                System.out.println("Your Booking is confirmed-----------");
+                System.out.println("Your booking is confirm with"+x.name);
+                x.currentLocation=this.dispachLocation;
+                return;
+            }
+        }
+        System.out.println("Your booking is cancelled due to available of car in your dispach location");
     }
     void checkTaxi(){
-        
+        System.out.println("Checking taxi details");
+        System.out.println(taxi);
+        for(TaxiDetails x:taxi){
+            System.out.println(x.name+"  :  "+x.currentLocation);
+        }
+        return;
     }
-
-
 }
-
-
 class Main{
     public static void main(String[] args) {
+            loop();
+    }
+    public static void loop(){
         Scanner s=new Scanner(System.in);
-        System.out.println("Enter 1 for customer login\n 2 for taxi owner login");
+        System.out.println("Enter \n1.customer login\n2. taxi owner login\n3. Close application");
         int x=s.nextInt();
+        if(x==3)
+        return;
         System.out.print("Enter the User name:");
         String uname=s.next();
         System.out.print("Enter the Password:");
@@ -87,10 +115,25 @@ class Main{
             while(tb.getAction()){}
         }
         else{
-            TaxiAllocation ta=new TaxiAllocation();
-            ta.addDetails();
-            while(tb.getAction()){}
+            
+            boolean x1=true;
+            while(x1){
+                System.out.println("1. Add taxi\n2. View taxi\n3. Exit");
+
+                switch(s.nextInt()){
+                    case 1:
+                    tb.addDetails();
+                    break;
+                    case 2:
+                    tb.checkTaxi();
+                    break;
+                    case 3:
+                    x1=false;
+                    System.out.println("Thank you for your visit");
+                    break;
+                }
+            }
         }
-       
+        loop();
     }
 }
